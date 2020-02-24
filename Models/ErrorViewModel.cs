17d6d7875e7445505b295f7afee9ddffdf3c95d7
Valmnt <valentin.mont@ynov.com>
2020-02-24 10:16:10 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VeloVMONT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
